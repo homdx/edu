30760e28d71ba9e39a6849888437592115298620
Sudip Sinha <sudipsinha.bappa@gmail.com>
2015-04-09 12:20:01 +0200

    * Function restructuring

---
+++ MathMods/Thesis/tr_crr.py
@@ -0,0 +1,20 @@
+__author__ = 'Sudip Sinha'
+
+
+from math import exp, sqrt
+
+
+def tr_underlying(s0: float, sigma: float, t: float, n: int) -> list:
+	"""Generate the tree of stock prices"""
+
+	s = [[0] * (i+1) for i in range(n+1)]
+	s[0][0] = s0
+
+	dt = t / n
+	u = exp(sigma * sqrt(dt))
+
+	for i in range(1, n+1):
+		for j in range(i+1):
+			s[i][j] = s0 * u**(-i+2*j)
+
+	return s
